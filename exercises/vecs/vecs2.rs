70b8c93e8cb0aef9e284172bbddefa7494f4fd3d
yu <hejunlbbc@gmail.com>
2025-01-24 16:13:24 +0000

    update

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -7,26 +7,32 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *element *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
     v
 }
 
+//vec_loop 函数
+//由于它是在原向量上进行修改，不会额外分配新的内存来存储结果，因此内存使用相对较低。
+//它只是对原向量中的元素进行修改，不会增加额外的内存开销。
+
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        element * 2
     }).collect()
 }
+/*vec_map 函数
+会创建一个新的向量来存储处理后的结果，这意味着它需要额外分配与原向量相同大小的内存空间。
+因此，内存使用相对较高，尤其是当向量较大时，会占用更多的内存。*/
 
 #[cfg(test)]
 mod tests {
